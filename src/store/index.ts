4317c75a10b24ef9af3f34daa2fd7040b9df54ed
designus <info@designus.lt>
2021-09-09 16:59:48 +0200

    add API and typed store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,39 @@
-import { createStore } from 'vuex';
+import { InjectionKey } from 'vue';
+import { createStore, createLogger, Store, useStore as baseUseStore } from 'vuex';
+import { Movie } from '@/api/types';
 
-export default createStore({
+const debug = process.env.NODE_ENV !== 'production';
+
+export interface State {
+  isLoading: boolean;
+  error: string | null;
+  data: {
+    searchResults: Movie[];
+    selectedMovies: Record<string, Movie>; 
+  }
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
   state: {
+    isLoading: false,
+    error: null,
+    data: {
+      searchResults: [],
+      selectedMovies: {}
+    }
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  // mutations: {
+  // },
+  // actions: {
+  // },
+  // modules: {
+  // },
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
 });
+
+export function useStore() {
+  return baseUseStore(key);
+}
